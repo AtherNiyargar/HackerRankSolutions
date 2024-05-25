78bbda203a863b0b17a7fc3f25ea9c0646685f2c
Ather <87631212+AtherNiyargar@users.noreply.github.com>
2024-05-25 09:37:12 +0530

    Create Functions.cpp

---
+++ C++/Functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int largest = a;
+
+    if (largest < b)
+        largest = b;
+
+    if (largest < c)
+        largest = c;
+
+    if (largest < d)
+        largest = d;
+
+    return largest;
+}
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+
+    return 0;
+}
